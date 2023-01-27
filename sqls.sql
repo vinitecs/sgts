56de55275e6c1be8bc3f9dc25507473ec6d5c7c8
Vinicius Lopes <viniciusamalia@gmail.com>
2023-01-27 10:14:29 -0300

    Update sqls.sql

--- sqls.sql
+++ sqls.sql
@@ -9,6 +9,17 @@ CREATE TABLE TAREFA(
 );
 
 
-INSERT INTO TAREFA(TAREFA_DESCRICAO,TAREFA_PRIORIDADE,TAREFA_SITUACAO) VALUES ('Exercicios as 15hs','ALTA','PENDENTE');
+INSERT INTO
+	TAREFA(TAREFA_DESCRICAO,TAREFA_PRIORIDADE,TAREFA_SITUACAO) 
+	VALUES ('Exercicios as 15hs','ALTA','PENDENTE'),
+	('Exercicios as 15hs','ALTA','PENDENTE'),
+	('Faculdade as 13hs','ALTA','EXECUCAO'),
+	('Jogo de futebol','ALTA','CONCLUIDO'),
+	('Exercicios as 13hs','MEDIA','CONCLUIDO'),
+	('Exercicios as 15hs','MEDIA','CONCLUIDO'),
+	('Exercicios as 15hs','ALTA','PENDENTE'),
+	('Exercicios as 15hs','MEDIA','PENDENTE')
+	
+
 
 SELECT * FROM TAREFA;
